37f70003254506ff7e7ffcb517a9e38b33cdde14
Tish Chungoora <tishal82@gmail.com>
2019-09-02 16:27:30 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,23 @@
+# Method 1
+
 def square_array(array)
-  # your code here
+  counter = 0
+  new_array = []
+  
+  while array[counter] do
+    new_array.push(array[counter]**2)
+    counter += 1
+  end
+  return new_array
 end
+
+
+=begin
+# Method 2
+
+def square_array(array)
+  new_array = Array.new(array.length)
+  array.each_index { |i| new_array[i] = array[i]**2  }
+  return new_array
+end
+=end
