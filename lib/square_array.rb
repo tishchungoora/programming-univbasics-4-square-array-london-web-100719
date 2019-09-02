e7c4bd1e6b0fbc5a0a1506926b794251b6d8476c
Tish Chungoora <tishal82@gmail.com>
2019-09-02 16:18:33 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,20 +1,22 @@
 # Method 1
-
+=begin
 def square_array(array)
   counter = 0
   new_array = []
   
   while array[counter] do
-    new_array.push(array[counter] **2)
+    new_array.push(array[counter]**2)
     counter += 1
   end
   return new_array
 end
+=end
 
 
 # Method 2
 
 def square_array(array)
   new_array = Array.new(array.length)
-  array.each_index { |i| a[i] = ary[i]  }
+  array.each_index { |i| new_array[i] = array[i]**2  }
+  return new_array
 end
